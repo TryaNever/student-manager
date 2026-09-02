1d13f5551ebeb978c4b5437e1873d6470fefd341
TryaNever <alexispernette@outlook.fr>
2026-09-02 23:35:14 +0200

    Initialize Student instance with sample data in main function

--- src/main.c
+++ src/main.c
@@ -6,6 +6,14 @@ int main()
 {
     int note = 10;
 
+    Student student;
+
+    student.id = 1;
+    student.first_name = "test";
+    student.last_name = "tested";
+    student.age = 15;
+    student.average = 15.7;
+
     note = note_add(note);
     printf("%d", note);
     system("PAUSE");
