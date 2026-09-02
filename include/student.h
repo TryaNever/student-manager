ff93d30f414d08f2ca9313e3521f10684642967c
TryaNever <alexispernette@outlook.fr>
2026-09-02 23:30:40 +0200

    Add Student struct definition to student.h

--- include/student.h
+++ include/student.h
@@ -1,6 +1,15 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
+typedef struct
+{
+    int id;
+    char *first_name;
+    char *last_name;
+    int age;
+    float average;
+} Student;
+
 int note_add(int note_choose);
 
 #endif
