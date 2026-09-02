b019c426dbbee63a6e25fb852d1efc7e7bf437b5
TryaNever <alexispernette@outlook.fr>
2026-09-02 23:49:57 +0200

    Refactor print_student function to display complete student details

--- src/student.c
+++ src/student.c
@@ -11,5 +11,10 @@ int note_add(int note_choose)
 
 void print_student(Student student)
 {
-    printf("%d", student.age);
+    printf("Student : \n");
+    printf("\tId : %d\n", student.id);
+    printf("\tPrenom : %s\n", student.first_name);
+    printf("\tNom : %s\n", student.last_name);
+    printf("\tAge : %d\n", student.age);
+    printf("\tNote Moyenne : %.2f\n", student.average);
 }
